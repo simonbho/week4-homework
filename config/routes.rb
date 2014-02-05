e834b9a93a0deef3a4ae7498cd918aa92fcd2e4c
simonbho <sho2015@kellogg.northwestern.edu>
2014-02-05 10:10:16 -0600

    HW completed
    search and conditions page working. conditions include current, min,
    and max temp

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Week4hw::Application.routes.draw do
 
   get "/" => "pages#home"
 
-  # HINT: Add more routes here...
-
+  get "/weather/search" => "weather#search"
+  get "/weather/conditions" => "weather#conditions"
 
 
 end
